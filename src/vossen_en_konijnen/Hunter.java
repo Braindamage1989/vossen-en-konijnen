e471441d3600e71cd75f3018fd1c0b5e002ee947
Braindamage1989 <R.Scholten@Live.nl>
2015-01-17 12:18:26 +0100

    Hunter klasse toevoegd, mager in functionaliteit

---
+++ src/vossen_en_konijnen/Hunter.java
@@ -0,0 +1,135 @@
+package vossen_en_konijnen; 
+
+import java.util.List;
+import java.util.Iterator;
+import java.util.Random;
+
+/**
+ * A simple model of a fox.
+ * Foxes age, move, eat rabbits, and die.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @version 2011.07.31
+ */
+public class Hunter implements Actor
+{    
+    // Individual characteristics (instance fields).
+    // The hunter his field
+    private Field field;
+    // The hunter's position in the field.
+    private Location location;
+    // Determine if the hunter is alive
+    private boolean alive;
+
+    /**
+     * Create a fox. A fox can be created as a new born (age zero
+     * and not hungry) or with a random age and food level.
+     * 
+     * @param randomAge If true, the fox will have random age and hunger level.
+     * @param field The field currently occupied.
+     * @param location The location within the field.
+     */
+    public Hunter(Field field, Location location)
+    {
+        alive = true;
+        this.field = field;
+        setLocation(location);
+    }
+    
+    /**
+     * This is what the fox does most of the time: it hunts for
+     * rabbits. In the process, it might breed, die of hunger,
+     * or die of old age.
+     * @param field The field currently occupied.
+     * @param newFoxes A list to return newly born foxes.
+     */
+    public void act(List<Actor> newHunter)
+    {
+            Location newLocation = findFood();
+            if(newLocation == null) { 
+                // No food found - try to move to a free location.
+                newLocation = getField().freeAdjacentLocation(getLocation());
+            }
+            // See if it was possible to move.
+            if(newLocation != null) {
+                setLocation(newLocation);
+            }
+    }
+    
+    public boolean isActive()
+    {
+        return alive;
+    }
+    
+    /**
+     * Look for rabbits adjacent to the current location.
+     * Only the first live rabbit is eaten.
+     * @return Where food was found, or null if it wasn't.
+     */
+    private Location findFood()
+    {
+        Field field = getField();
+        List<Location> adjacent = field.adjacentLocations(getLocation());
+        Iterator<Location> it = adjacent.iterator();
+        while(it.hasNext()) {
+            Location where = it.next();
+            Object animal = field.getObjectAt(where);
+            if(animal instanceof Rabbit) {
+                Rabbit rabbit = (Rabbit) animal;
+                if(rabbit.isActive()) { 
+                    rabbit.setDead();
+                    // Remove the dead rabbit from the field.
+                    return where;
+                }
+            }
+            else if(animal instanceof Fox) {
+                Fox fox = (Fox) animal;
+                if(fox.isActive()) { 
+                    fox.setDead();
+                    // Remove the dead rabbit from the field.
+                    return where;
+                }
+            }
+            else if(animal instanceof Lynx) {
+                Lynx lynx = (Lynx) animal;
+                if(lynx.isActive()) { 
+                    lynx.setDead();
+                    // Remove the dead rabbit from the field.
+                    return where;
+                }
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * Return the animal's location.
+     * @return The animal's location.
+     */
+    protected Location getLocation()
+    {
+        return location;
+    }
+    
+    /**
+     * Place the animal at the new location in the given field.
+     * @param newLocation The animal's new location.
+     */
+    protected void setLocation(Location newLocation)
+    {
+        if(location != null) {
+            field.clear(location);
+        }
+        location = newLocation;
+        field.place(this, newLocation);
+    }
+    
+    /**
+     * Return the animal's field.
+     * @return The animal's field.
+     */
+    protected Field getField()
+    {
+        return field;
+    }
+}
