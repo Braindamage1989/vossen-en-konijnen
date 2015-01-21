90746ef96e0dd037adedb13cadde295d6b1dcc85
Braindamage1989 <R.Scholten@Live.nl>
2015-01-21 19:34:42 +0100

    Lynx stats aanpassingen

--- src/vossen_en_konijnen/logic/Lynx.java
+++ src/vossen_en_konijnen/logic/Lynx.java
@@ -18,14 +18,14 @@ public class Lynx extends Animal
     // The age at which a lynx can start to breed.
     private static final int BREEDING_AGE = 25;
     // The age to which a lynx can live.
-    private static final int MAX_AGE = 200;
+    private static final int MAX_AGE = 150;
     // The likelihood of a lynx breeding.
-    private static final double BREEDING_PROBABILITY = 0.05;
+    private static final double BREEDING_PROBABILITY = 0.08;
     // The maximum number of births.
-    private static final int MAX_LITTER_SIZE = 2;
+    private static final int MAX_LITTER_SIZE = 1;
     // The food value of a single rabbit. In effect, this is the
     // number of steps a lynx can go before it has to eat again.
-    private static final int FOOD_VALUE = 15;
+    private static final int FOOD_VALUE = 13;
     // A shared random number generator to control breeding.
     private static final Random rand = Randomizer.getRandom();
     
