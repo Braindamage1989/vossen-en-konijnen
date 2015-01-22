4a7d07997c0ffe3484cd2f4ddcee145285558c4c
weetok <weetok@live.nl>
2015-01-22 10:24:03 +0100

    FieldView is nu apparte klasse

---
+++ src/vossen_en_konijnen/view/FieldView.java
@@ -0,0 +1,78 @@
+package vossen_en_konijnen.view;
+
+import java.awt.*;
+import vossen_en_konijnen.logic.*;
+
+public class FieldView extends AbstractView
+{
+    
+
+    /**
+     * Create a new FieldView component.
+     */
+    public FieldView(FieldStats stats, int height, int width)
+    {
+    	super(stats);
+        gridHeight = height;
+        gridWidth = width;
+        size = new Dimension(0, 0);
+    }
+
+    /**
+     * Tell the GUI manager how big we would like to be.
+     */
+    public Dimension getPreferredSize()
+    {
+        return new Dimension(gridWidth * GRID_VIEW_SCALING_FACTOR,
+                             gridHeight * GRID_VIEW_SCALING_FACTOR);
+    }
+
+    /**
+     * Prepare for a new round of painting. Since the component
+     * may be resized, compute the scaling factor again.
+     */
+    public void preparePaint()
+    {
+        if(! size.equals(getSize())) {  // if the size has changed...
+            size = getSize();
+            fieldImage = this.createImage(size.width, size.height);
+            g = this.getGraphics();
+
+            xScale = size.width / gridWidth;
+            if(xScale < 1) {
+                xScale = GRID_VIEW_SCALING_FACTOR;
+            }
+            yScale = size.height / gridHeight;
+            if(yScale < 1) {
+                yScale = GRID_VIEW_SCALING_FACTOR;
+            }
+        }
+    }
+    
+    /**
+     * Paint on grid location on this field in a given color.
+     */
+    public void drawMark(int x, int y, Color color)
+    {
+        g.setColor(color);
+        g.fillRect(x * xScale, y * yScale, xScale-1, yScale-1);
+    }
+
+    /**
+     * The field view component needs to be redisplayed. Copy the
+     * internal image to screen.
+     */
+    public void paintComponent(Graphics g)
+    {
+        if(fieldImage != null) {
+            Dimension currentSize = getSize();
+            if(size.equals(currentSize)) {
+                g.drawImage(fieldImage, 0, 0, null);
+            }
+            else {
+                // Rescale the previous image.
+                g.drawImage(fieldImage, 0, 0, currentSize.width, currentSize.height, null);
+            }
+        }
+    }
+}
