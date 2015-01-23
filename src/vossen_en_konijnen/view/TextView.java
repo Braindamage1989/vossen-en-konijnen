e79deae24f564fa1080c96a2fd8916250da25329
Frank Mulder <w.frank.mulder@gmail.com>
2015-01-23 14:31:24 +0100

    textview ook aangepast

--- src/vossen_en_konijnen/view/TextView.java
+++ src/vossen_en_konijnen/view/TextView.java
@@ -2,7 +2,7 @@ package vossen_en_konijnen.view;
 
 import javax.swing.*;
 
-import vossen_en_konijnen.controller.SimulatorView;
+import vossen_en_konijnen.controller.*;
 import vossen_en_konijnen.model.Field;
 import vossen_en_konijnen.model.FieldStats;
 
@@ -18,7 +18,7 @@ public class TextView extends AbstractView
 	private static final long serialVersionUID = 522169609862382099L;
 	private JLabel population;
 	
-	public TextView(SimulatorView model, FieldStats stats, int height, int depth)
+	public TextView(Controller model, FieldStats stats, int height, int depth)
 	{
 		super(model, stats, height, depth);
 		population = new JLabel("", JLabel.CENTER);
