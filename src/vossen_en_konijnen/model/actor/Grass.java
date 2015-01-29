aece540bbeb6a90e45d70338de0a3faa5ec872a2
Braindamage1989 <R.Scholten@Live.nl>
2015-01-29 10:12:22 +0100

    Betere naamgeving voor sommige methodes en variabeles + javadoc

--- src/vossen_en_konijnen/model/actor/Grass.java
+++ src/vossen_en_konijnen/model/actor/Grass.java
@@ -8,45 +8,47 @@ import vossen_en_konijnen.model.Location;
 import vossen_en_konijnen.model.Randomizer;
 
 /**
- * A simple model of a rabbit.
- * Rabbits age, move, breed, and die.
+ * A simple model of a grass.
+ * Grass age, multiply and die.
  * 
- * @author David J. Barnes and Michael Kölling
+ * @author Bor D., Mulder F., Scholten R.
  * @version 2011.07.31
  */
 public class Grass implements Actor
 {
-    // Characteristics shared by all rabbits (class variables).
+    // Characteristics shared by all grass (class variables).
     
-    // The hunter his field
+    // The grass his field
     private Field field;
-    // The hunter's position in the field.
+    // The grasses position in the field.
     private Location location;
-    // Determine if the hunter is alive
-    private boolean alive;
+    // Determine if the grass is active
+    private boolean active;
+    // Determine the age of the grass
     private int age;
     
-    private static final int BREEDING_AGE = 3;
-    // The age to which a rabbit can live.
+    // The age grass can multiply
+    private static final int MULTIPLYING_AGE = 3;
+    // The age to which a grass can live.
     private static final int MAX_AGE = 15;
-    // The likelihood of a rabbit breeding.
-    private static final double BREEDING_PROBABILITY = 0.5;
-    // The maximum number of births.
-    private static final int MAX_LITTER_SIZE = 8;
-    // A shared random number generator to control breeding.
+    // The likelihood of a grass breeding.
+    private static final double MULTIPLYING_PROBABILITY = 0.5;
+    // The maximum number of multiplies.
+    private static final int MAX_MULTIPLYING_SIZE = 8;
+    // A shared random number generator to control multiplying.
     private static final Random rand = Randomizer.getRandom();
 
     /**
-     * Create a new rabbit. A rabbit may be created with age
+     * Create a new grass. A grass may be created with age
      * zero (a new born) or with a random age.
      * 
-     * @param randomAge If true, the rabbit will have a random age.
+     * @param randomAge If true, the grass will have a random age.
      * @param field The field currently occupied.
      * @param location The location within the field.
      */
     public Grass(boolean randomAge, Field field, Location location)
     {
-        alive = true;
+        active = true;
         this.field = field;
         setLocation(location);
         setAge(0);
@@ -58,19 +60,24 @@ public class Grass implements Actor
     /**
      * This is what the rabbit does most of the time - it runs 
      * around. Sometimes it will breed or die of old age.
-     * @param newRabbits A list to return newly born rabbits.
+     * @param newGrass A list to return newly born rabbits.
      */
+    @Override
     public void act(List<Actor> newGrass)
     {
         incrementAge();
         if(isActive()) {
-            giveBirth(newGrass);            
+            goMultiply(newGrass);            
         }
     }
     
+    /**
+     * Indicate that the grass is no longer alive.
+     * It is removed from the field.
+     */
     public void setDead()
     {
-        alive = false;
+        active = false;
         if(location != null) {
             field.clear(location);
             location = null;
@@ -79,8 +86,8 @@ public class Grass implements Actor
     }
     
     /**
-     * Place the hunter at the new location in the given field.
-     * @param newLocation The hunters new location.
+     * Place the grass at the new location in the given field.
+     * @param newLocation The grasses new location.
      */
     private void setLocation(Location newLocation)
     {
@@ -91,14 +98,18 @@ public class Grass implements Actor
         field.place(this, newLocation);
     }
     
+    /*
+     * Setter for the grasses age
+     * @param age The new age to be setted for grasses
+     */
     private void setAge(int age)
     {
         this.age = age;
     }
     
     /**
-     * Return the animal's field.
-     * @return The animal's field.
+     * Return the grasses field.
+     * @return The grasses field.
      */
     private Field getField()
     {
@@ -106,7 +117,7 @@ public class Grass implements Actor
     }
     
     /**
-     * Increase the age. This could result in the fox's death.
+     * Increase the age. This could result in the grasses death.
      */
     private void incrementAge()
     {
@@ -117,27 +128,32 @@ public class Grass implements Actor
     }
     
     /**
-     * Generate a number representing the number of births,
-     * if it can breed.
-     * @return The number of births (may be zero).
+     * Generate a number representing the number of multiplies,
+     * if it can multiply.
+     * @return The number of multiplies (may be zero).
      */
-    private int breed()
+    private int multiply()
     {
-        int births = 0;
-        if(canBreed() && rand.nextDouble() <= BREEDING_PROBABILITY) {
-            births = rand.nextInt(MAX_LITTER_SIZE) + 1;
+        int newGrass = 0;
+        if(canMultiply() && rand.nextDouble() <= MULTIPLYING_PROBABILITY) {
+            newGrass = rand.nextInt(MAX_MULTIPLYING_SIZE) + 1;
         }
-        return births;
+        return newGrass;
     }
     
-    private void giveBirth(List<Actor> newGrass)
+    /**
+     * Check whether or not this grass is to multiply at this step.
+     * New multiplies will be made into free adjacent locations.
+     * @param newGrass A list to return newly created grasses.
+     */
+    private void goMultiply(List<Actor> newGrass)
     {
-        // New foxes are born into adjacent locations.
+        // New grasses are born into adjacent locations.
         // Get a list of adjacent free locations.
         Field field = getField();
         List<Location> free = field.getFreeAdjacentLocations(getLocation());
-        int births = breed();
-        for(int b = 0; b < births && free.size() > 0; b++) {
+        int youngGrass = multiply();
+        for(int b = 0; b < youngGrass && free.size() > 0; b++) {
             Location loc = free.remove(0);
             Grass young = new Grass(false, field, loc);
             newGrass.add(young);
@@ -145,21 +161,30 @@ public class Grass implements Actor
     }
     
     /**
-     * Return the hunters location.
-     * @return The hunters location.
+     * Return the grasses location.
+     * @return The grasses location.
      */
     private Location getLocation()
     {
         return location;
     }
     
-    protected boolean canBreed()
+    /**
+     * Return if grass can multiply.
+     * @return if grass can multiply.
+     */
+    protected boolean canMultiply()
     {
-        return age >= BREEDING_AGE;
+        return age >= MULTIPLYING_AGE;
     }
     
+    /**
+     * Return if grasses are still active
+     * @return if grasses are still active.
+     */
+    @Override
     public boolean isActive()
     {
-        return alive;
+        return active;
     }
 }
